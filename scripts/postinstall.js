466903b90a1a8d533919642af00e98bda10df750
eliy <eliy@sqream.com>
2018-08-23 12:11:06 +0300

    cross platform postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,9 +1,17 @@
-#!/usr/bin/env node
-var fs = require('fs-extra');
+const fs = require('fs-extra');
+require('path')
 
-var HOME_DIR =  __dirname + '/';
-const version  = parseInt(Number(process.version.match(/^v(\d+\.\d+)/)[1]));
+const node_version  = parseInt(Number(process.version.match(/^v(\d+\.\d+)/)[1]));  
+const addon_path = (node_version === 6) ? path.resolve('lib', 'addon_6') : path.resolve('lib', 'addon_8') // path.resolve starts with current folder
 
+fs.copy(addon_path, path.resolve('lib', 'addon'), function (err) {
+    if (err) throw err;
+    console.log('renamed complete');
+});
+//deleteFolderRecursive(HOME_DIR+'../lib/addon_8');
+
+
+// Not currently used, not cross platform since resolves paths with + "/" ..
 var deleteFolderRecursive = function(path) {
     if (fs.existsSync(path)) {
         fs.readdirSync(path).forEach(function(file, index){
@@ -19,17 +27,5 @@ var deleteFolderRecursive = function(path) {
 };
 
 
-if (version === 6) {
-    fs.copy(HOME_DIR+'../lib/addon_6', HOME_DIR+'../lib/addon', function (err) {
-        if (err) throw err;
-        console.log('renamed complete');
-    });
-    //deleteFolderRecursive(HOME_DIR+'../lib/addon_8');
-} else {
-    fs.copy(HOME_DIR+'../lib/addon_8', HOME_DIR+'../lib/addon', function (err) {
-        if (err) throw err;
-        console.log('renamed complete');
-    });
-    //deleteFolderRecursive(HOME_DIR+'../lib/addon_6');
-}
+
 
